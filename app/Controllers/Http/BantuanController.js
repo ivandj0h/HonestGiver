98ffefeedc40e4baf69da492cd8a4737f80aecd4
ivandi1980 <ivandi.djoh@gmail.com>
2019-09-23 17:28:07 +0800

    modified views home and added functional controller

--- app/Controllers/Http/BantuanController.js
+++ app/Controllers/Http/BantuanController.js
@@ -1,17 +1,21 @@
 'use strict'
 
+const Bantuan = use('App/Models/Bantuan')
+
 class BantuanController {
     
     async home({ view }) {
 
-      const cthData = [
-        {name: 'juna',age: 5},
-        {name: 'dave',age: 11}
-      ]
+      // const cthData = [
+      //   {name: 'juna',age: 5},
+      //   {name: 'dave',age: 11}
+      // ]
+
+    const bantuan = await Bantuan.all()
 
         return view.render('home', {
           title: 'BANTUAN DIBUTUHKAN',
-          data:cthData
+          data: bantuan.toJSON()
         })
       }
 }
