0a627a77fdf4c5b8e0e789cf898cc5ac83cefaa3
ivandi1980 <ivandi.djoh@gmail.com>
2019-09-24 08:40:28 +0800

    mod links

--- src/routes/links.js
+++ src/routes/links.js
@@ -9,22 +9,17 @@ router.get('/', isLoggedIn, async (req, res) => {
     res.render('links/list', { links });
 });
 
-router.get('/', async (req, res) => {
-    const terpenuhi = await pool.query('SELECT bantuan_givers.tanggal_dibantu, bantuan_givers.nilai_bantuan, useraskers.name, bantuans.tanggal, bantuans.judul FROM bantuan_givers, useraskers, bantuans WHERE bantuan_givers.bantuan_id = bantuans.id AND bantuans.useraskers_id = useraskers.id');
-    res.render('links/edit', { terpenuhi });
-});
-
-router.post('/edit/:id', async (req, res) => {
-    const { id } = req.params;
-    const { title, description, url} = req.body; 
-    const newLink = {
-        title,
-        description,
-        url
-    };
-    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
-    req.flash('success', 'Link Updated Successfully');
-    res.redirect('/links');
-});
+// router.post('/edit/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const { title, description, url} = req.body; 
+//     const newLink = {
+//         title,
+//         description,
+//         url
+//     };
+//     await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+//     req.flash('success', 'Link Updated Successfully');
+//     res.redirect('/links');
+// });
 
 module.exports = router;
