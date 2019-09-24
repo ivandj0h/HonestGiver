460d6f93041cacf362a5e3ee88b30242a592b0fe
ivandi1980 <ivandi.djoh@gmail.com>
2019-09-24 08:58:52 +0800

    add moment

--- src/routes/link.js
+++ src/routes/link.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
+const moment = require('moment');
+moment().format();
+
 router.get('/', isLoggedIn, async (req, res) => {
     const link = await pool.query('SELECT bantuan_givers.tanggal_dibantu, bantuan_givers.nilai_bantuan, useraskers.name, bantuans.tanggal, bantuans.judul FROM bantuan_givers, useraskers, bantuans WHERE bantuan_givers.bantuan_id = bantuans.id AND bantuans.useraskers_id = useraskers.id');
     res.render('link/list', { link });
