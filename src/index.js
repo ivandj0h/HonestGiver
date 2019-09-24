8cbcb1855089315558f9ce7ee4256ee0f00f3122
ivandi1980 <ivandi.djoh@gmail.com>
2019-09-24 08:39:04 +0800

    mod link

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ app.use((req, res, next) => {
 app.use(require('./routes/index'));
 app.use(require('./routes/authentication'));
 app.use('/links', require('./routes/links'));
+app.use('/link', require('./routes/link'));
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ src/routes/link.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+const { isLoggedIn } = require('../lib/auth');
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const link = await pool.query('SELECT bantuan_givers.tanggal_dibantu, bantuan_givers.nilai_bantuan, useraskers.name, bantuans.tanggal, bantuans.judul FROM bantuan_givers, useraskers, bantuans WHERE bantuan_givers.bantuan_id = bantuans.id AND bantuans.useraskers_id = useraskers.id');
+    res.render('link/list', { link });
+});
+
+module.exports = router;
