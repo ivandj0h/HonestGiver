7c22b9c0d2a01818fdccd1e37b99dbff5c4de5d3
ivandi1980 <ivandi.djoh@gmail.com>
2019-09-24 09:42:29 +0800

    added details

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,8 +5,15 @@ const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
 router.get('/', isLoggedIn, async (req, res) => {
-    const links = await pool.query('SELECT bantuans.tanggal, useraskers.name, bantuans.judul, bantuans.jumlah_bantuan, bantuan_givers.nilai_bantuan FROM bantuans, useraskers, bantuan_givers WHERE bantuans.useraskers_id = useraskers.id AND bantuan_givers.bantuan_id = bantuans.id');
+    const links = await pool.query('SELECT bantuans.id, bantuans.tanggal, useraskers.name, bantuans.judul, bantuans.jumlah_bantuan, bantuan_givers.nilai_bantuan FROM bantuans, useraskers, bantuan_givers WHERE bantuans.useraskers_id = useraskers.id AND bantuan_givers.bantuan_id = bantuans.id');
     res.render('links/list', { links });
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT bantuans.id, bantuans.tanggal, useraskers.name, bantuans.judul, bantuans.detail, bantuans.jumlah_bantuan, bantuan_givers.nilai_bantuan FROM bantuans, useraskers, bantuan_givers WHERE bantuans.useraskers_id = useraskers.id AND bantuan_givers.bantuan_id = ?', [id]);
+    console.log(links);
+    res.render('links/edit', {link: links[0]});
 });
 
 // router.post('/edit/:id', async (req, res) => {
